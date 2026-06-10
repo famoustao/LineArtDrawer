dbbab915d165c3c332666b923efc1f287a98e599
famoustao <1013177757@qq.com>
2026-06-11 00:39:26 +0800

    Add files via upload

--- src/input/HotkeyManager.cpp
+++ src/input/HotkeyManager.cpp
@@ -1,4 +1,5 @@
 #include "HotkeyManager.h"
+#include <QString>
 #include <iostream>
 
 #ifdef _WIN32
@@ -18,29 +19,49 @@ HotkeyManager::~HotkeyManager() {
 }
 
 bool HotkeyManager::registerHotkey(int vkCode, unsigned int modifiers, HotkeyCallback callback) {
-#ifdef _WIN32
+    return registerHotkeyDynamic(vkCode, modifiers, callback) > 0;
+}
+
+int HotkeyManager::registerHotkeyDynamic(int vkCode, unsigned int modifiers, HotkeyCallback callback) {
+    std::lock_guard<std::mutex> lock(hotkeysMutex_);
     int hotkeyId = nextHotkeyId_++;
-    
+
+#ifdef _WIN32
     // 转换修饰键为Windows格式
     UINT fsModifiers = 0;
     if (modifiers & MOD_ALT)       fsModifiers |= MOD_ALT;
     if (modifiers & MOD_CONTROL)   fsModifiers |= MOD_CONTROL;
     if (modifiers & MOD_SHIFT)     fsModifiers |= MOD_SHIFT;
-    
+
     if (RegisterHotKey(nullptr, hotkeyId, fsModifiers, vkCode)) {
         hotkeys_.push_back({vkCode, modifiers, hotkeyId, callback});
-        return true;
+        return hotkeyId;
     } else {
         std::cerr << "HotkeyManager: 注册热键失败, 错误码: " << GetLastError() << std::endl;
-        return false;
+        return -1;
     }
 #else
-    hotkeys_.push_back({vkCode, modifiers, nextHotkeyId_++, callback});
-    return true;
+    hotkeys_.push_back({vkCode, modifiers, hotkeyId, callback});
+    return hotkeyId;
 #endif
 }
 
+bool HotkeyManager::unregisterHotkey(int hotkeyId) {
+    std::lock_guard<std::mutex> lock(hotkeysMutex_);
+    for (auto it = hotkeys_.begin(); it != hotkeys_.end(); ++it) {
+        if (it->atomId == hotkeyId) {
+#ifdef _WIN32
+            UnregisterHotKey(nullptr, it->atomId);
+#endif
+            hotkeys_.erase(it);
+            return true;
+        }
+    }
+    return false;
+}
+
 void HotkeyManager::unregisterAll() {
+    std::lock_guard<std::mutex> lock(hotkeysMutex_);
 #ifdef _WIN32
     for (const auto& hk : hotkeys_) {
         UnregisterHotKey(nullptr, hk.atomId);
@@ -53,6 +74,36 @@ void HotkeyManager::stop() {
     running_ = false;
 }
 
+QString HotkeyManager::hotkeyName(int vkCode, unsigned int modifiers) {
+    QString name;
+    if (modifiers & MOD_CONTROL) name += "Ctrl+";
+    if (modifiers & MOD_SHIFT)   name += "Shift+";
+    if (modifiers & MOD_ALT)     name += "Alt+";
+
+#ifdef _WIN32
+    if (vkCode >= VK_F1 && vkCode <= VK_F24) {
+        name += "F" + QString::number(vkCode - VK_F1 + 1);
+    } else if (vkCode == VK_SPACE) {
+        name += "Space";
+    } else if (vkCode == VK_ESCAPE) {
+        name += "ESC";
+    } else if (vkCode == VK_RETURN) {
+        name += "Enter";
+    } else if (vkCode >= 0x30 && vkCode <= 0x5A) {
+        name += QChar(vkCode);
+    } else {
+        name += "Key(" + QString::number(vkCode) + ")";
+    }
+#else
+    // Linux: 简单显示键码
+    if (vkCode == 0xFFBF) name += "F7";
+    else if (vkCode == 0xFFC0) name += "F8";
+    else if (vkCode == 0xFF08) name += "Backspace";
+    else name += "Key(" + QString::number(vkCode) + ")";
+#endif
+    return name;
+}
+
 void HotkeyManager::run() {
 #ifdef _WIN32
     running_ = true;
@@ -63,6 +114,7 @@ void HotkeyManager::run() {
         if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
             if (msg.message == WM_HOTKEY) {
                 int hotkeyId = static_cast<int>(msg.wParam);
+                std::lock_guard<std::mutex> lock(hotkeysMutex_);
                 for (size_t i = 0; i < hotkeys_.size(); ++i) {
                     if (hotkeys_[i].atomId == hotkeyId) {
                         emit hotkeyTriggered(static_cast<int>(i));
@@ -74,7 +126,7 @@ void HotkeyManager::run() {
                 }
             }
         }
-        
+
         // 短暂休眠避免CPU占用过高
         QThread::msleep(5);
     }
--- src/input/HotkeyManager.h
+++ src/input/HotkeyManager.h
@@ -4,6 +4,7 @@
 #include <QThread>
 #include <atomic>
 #include <functional>
+#include <mutex>
 
 #ifdef _WIN32
 #include <windows.h>
@@ -24,12 +25,21 @@ public:
 
     // 注册热键（Windows虚拟键码 + 修饰键）
     bool registerHotkey(int vkCode, unsigned int modifiers, HotkeyCallback callback);
-    
+
+    // 动态注册热键（返回热键ID，用于后续注销）
+    int registerHotkeyDynamic(int vkCode, unsigned int modifiers, HotkeyCallback callback);
+
+    // 注销指定热键
+    bool unregisterHotkey(int hotkeyId);
+
     // 注销所有热键
     void unregisterAll();
-    
+
     // 停止监听
     void stop();
+
+    // 获取热键名称（用于UI显示）
+    static QString hotkeyName(int vkCode, unsigned int modifiers);
 
 signals:
     void hotkeyTriggered(int id);
@@ -39,6 +49,7 @@ protected:
 
 private:
     std::atomic<bool> running_;
+    std::mutex hotkeysMutex_;
     struct HotkeyInfo {
         int vkCode;
         unsigned int modifiers;
