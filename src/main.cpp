94478e55965370e0ed46ff33abf661c4099618db
famoustao <1013177757@qq.com>
2026-06-10 22:59:58 +0800

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[]) {
     // 设置应用程序信息
     app.setApplicationName("SketchMaster");
     app.setOrganizationName("SketchMaster");
-    app.setApplicationDisplayName("SketchMaster - 图片转线稿与自动绘制工具");
+    app.setApplicationDisplayName("图片转矢量图自动绘图工具，涛2026年6月编译");
     
     // 创建主窗口
     SketchMaster::MainWindow window;
