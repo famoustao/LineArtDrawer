67197b3f844305690c9c33a105d9a65545841019
famoustao <1013177757@qq.com>
2026-06-13 09:16:59 +0800

    Add files via upload

--- src/core/HEDDetector.h
+++ src/core/HEDDetector.h
@@ -3,7 +3,6 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/dnn.hpp>
 #include <string>
-#include <vector>
 
 namespace SketchMaster {
 
@@ -33,27 +32,6 @@ private:
 
     // 在多个候选路径中查找模型文件
     std::string findModelFile(const std::string& modelDir, const std::string& filename);
-};
-
-// CropLayer 自定义层
-// HED 的 Caffe prototxt 中使用了 Crop 层，OpenCV DNN 不原生支持，需要自定义实现。
-// 必须在 readNet 之前通过 LayerFactory 注册。
-class CropLayer : public cv::dnn::Layer {
-public:
-    // 工厂方法，用于 LayerFactory 注册
-    static cv::Ptr<cv::dnn::Layer> create(cv::dnn::LayerParams& params);
-
-    // 计算输出形状
-    // inputs[0] = 输入特征图, inputs[1] = 目标尺寸参考
-    // 输出 = 从 inputs[0] 中心裁剪到 inputs[1] 的空间尺寸
-    virtual bool getMemoryShapes(const std::vector<std::vector<int>>& inputs,
-                                  const int requiredOutputs,
-                                  std::vector<std::vector<int>>& outputs) const override;
-
-    // 前向传播：执行裁剪操作
-    virtual void forward(const std::vector<cv::Mat*>& inputs,
-                         std::vector<cv::Mat>& outputs,
-                         std::vector<cv::Mat>& internals) override;
 };
 
 } // namespace SketchMaster
